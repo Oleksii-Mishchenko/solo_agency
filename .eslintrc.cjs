04c1950ad78c0fe1c7f32c282d42f7309d922d91
OleksiiMishchenko <alex.mishch@gmail.com>
2023-12-19 17:29:35 +0200

    Hello world added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -5,11 +7,20 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+  ],
   rules: {
+    'prettier/prettier': ['error', { semi: true }],
+    'prettier/prettier': ['error', { 'singleQuote': true }],
+    'max-len': ['error', { 'code': 80, 'ignoreComments': true, 'ignoreTemplateLiterals': true }],
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/semi': ['error'],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/solo_agency/',
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./**/*.{ts,tsx}"',
+      },
+    }),
+  ],
+});
