e7e5956bc1df2192bd22ba8f4535f88f2a44456e
OleksiiMishchenko <alex.mishch@gmail.com>
2023-12-21 17:26:22 +0200

    Edited main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store.ts';
+// import { Provider } from 'react-redux';
+// import { store } from './app/store.ts';
 import { Root } from './Root.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <Root />
-    </React.StrictMode>
-  </Provider>,
+  // <Provider store={store}>
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  // </Provider>,
 );
